4f3f4aba0f3642aae1c639b549a6c149afc31970
Zak Warsame <zakwarsamee@gmail.com>
2021-08-25 19:37:37 +0000

    add constants.js and configure constants in clients.js

--- client.js
+++ client.js
@@ -1,10 +1,12 @@
+const { IP, PORT } = require("./constants");
 const net = require("net");
+
 
 // establishes a connection with the game server
 const connect = function () {
   const conn = net.createConnection({
-    host: '135.23.223.133', // IP address here,
-    port: '50542' // PORT number here,
+    host: IP, // IP address here,
+    port: PORT // PORT number here,
   });
 
   conn.on("data", (data) => {
---
+++ constants.js
@@ -0,0 +1,8 @@
+const IP = '135.23.223.133';
+const PORT = '50542';
+
+
+module.exports = {
+    IP,
+    PORT,
+  };    
